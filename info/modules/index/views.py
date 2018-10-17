c51e9c340817728fcf337e0f4ea332217b4997e5
lvcui <1273938985@qq.com>
2018-10-17 20:18:09 +0800

    注册跟路由

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,13 @@
+from flask import render_template, current_app
+
 from . import index_blu
 
+# 首页视图函数
 @index_blu.route('/')
 def index():
+    return render_template('index.html')
 
-    return "index"
+# 加载网页图标
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
